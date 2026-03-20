46114096a2935247f06ccdbd2e2b2fa0cf8e94fa
DavidQRy <davidgui393@gmail.com>
2026-03-19 21:42:20 -0500

    fix: soluciona conflictos de AppRoutes y App.css

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,22 +1,14 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-<<<<<<< HEAD
 import { Auth } from '../Auth.jsx';
-=======
-import { Login } from '../login.jsx';
->>>>>>> 0f6858d3893b97247c31b9c2e966cb1745d8d4ed
 import { LandingPage } from '../pages/LandingPage.jsx';
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
-<<<<<<< HEAD
-      <Route path="/Auth" element={<Auth />} />
-=======
-      <Route path="/login" element={<Login />} />
->>>>>>> 0f6858d3893b97247c31b9c2e966cb1745d8d4ed
+      <Route path='/' element={<LandingPage />} />
+      <Route path='/Auth' element={<Auth />} />
       {/* Catch-all: redirige a Landing */}
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path='*' element={<Navigate to='/' replace />} />
     </Routes>
   );
 }
